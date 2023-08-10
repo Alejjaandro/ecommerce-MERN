36f76a9f7038fcd5ed30bd8d5c0e82bb3c64d1f6
Alejjaandro <alejandroolaso98@gmail.com>
2023-08-10 20:44:15 +0200

    useData is a file with some repetitive code.

--- frontend/src/hooks/useData.js
+++ frontend/src/hooks/useData.js
@@ -1,27 +1,38 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-// ========== FUNCTION TO GET ALL PRODUCTS ========== //
-function useProducts() {
+// ========== FUNCTION ALL PRODUCTS & CATEGORY PRODUCTS ========== //
+function useProducts(category) {
 
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
+
         const getProducts = async () => {
 
             try {
                 // Make a get petition to the URL stablished in "/backend/routes/Product.js".
                 const res = await axios.get('http://localhost:4000/api/products');
-                setProducts(res.data);
+                const resProducts = res.data;
+
+                // If we call the function with a category as argument, then we filter the products
+                // and we save the one that matches the category.
+                if (category) {
+                    setProducts(resProducts.filter(prod => prod.category === category));
+
+                    // If not, then we save all products.
+                } else {
+                    setProducts(resProducts);
+                }
 
             } catch (error) {
-
+                console.log(error);
             }
-        };
+        }
 
         getProducts();
 
-    }, []);
+    }, [category]);
 
     return products;
 };
@@ -39,12 +50,12 @@ function useImages() {
                 const res = await axios.get('http://localhost:4000/api/products');
 
                 // We only want 5 of them for the slider.
-                const someProd = res.data.splice(0,5);
+                const someProd = res.data.splice(0, 5);
 
-                setImages( someProd.map( (prod) =>  prod.thumbnail) );
+                setImages(someProd.map((prod) => prod.thumbnail));
 
             } catch (error) {
-
+                console.log(error);
             }
         };
 
@@ -55,4 +66,89 @@ function useImages() {
     return images
 };
 
-export {useProducts, useImages};
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NOT WORKING !!!!!!!!!!!!!!!!!!!!!!!!!! */
+// ========== FUNCTION TO GET A PRODUCT FOR EACH CATEGORY ========== //
+function useProdForCategory() {
+
+    const [categories, setCategories] = useState([]);
+    const [prodForCategory, setProdForCategory] = useState([]);
+
+    useEffect(() => {
+
+        const getProdForCategory = async () => {
+
+            try {
+
+                const res = await axios.get('http://localhost:4000/api/products');
+                const products = res.data;
+
+                setCategories([...new Set(products.map((prod) => prod.category))]);
+
+                setProdForCategory(
+                    categories.map(category => {
+                        return products.find(product => product.category === category);
+                    })
+                );
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getProdForCategory();
+
+    }, []);
+
+    return prodForCategory;
+};
+
+// ========== FUNCTION TO GET CATEGORIES & BRANDS ========== //
+function useCategoriesAndBrands() {
+
+    const [products, setProducts] = useState([]);
+    const categoriasConMarcas = {};
+
+    useEffect(() => {
+
+        const getProducts = async () => {
+
+            try {
+
+                const res = await axios.get('http://localhost:4000/api/products');
+                setProducts(res.data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getProducts();
+
+    }, []);
+
+    products.forEach(product => {
+
+        const { category, brand } = product;
+
+        if (!categoriasConMarcas[category]) {
+
+            return categoriasConMarcas[category] = [brand];
+
+        } else if (!categoriasConMarcas[category].includes(brand)) {
+
+            return categoriasConMarcas[category].push(brand);
+
+        }
+    });
+
+   return categoriasConMarcas;
+};
+
+
+export {
+    useProducts,
+    useImages,
+    useProdForCategory,
+    useCategoriesAndBrands
+};
