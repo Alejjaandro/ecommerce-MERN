f12bb9f2ca2658a9cb37523f290089d349a8437f
Alejjaandro <alejandroolaso98@gmail.com>
2023-08-27 11:43:36 +0200

    Filter logic extracted to a FilterNavbar componnent.

---
+++ frontend/src/components/FilterNavbar.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+
+export default function FilterNavbar({ useProducts, onFilter }) {
+    // We extract what we need from context
+    const { getCategoriesAndBrands, categoriesAndBrands, brands, getCategories, categories } = useProducts();
+
+    const [categoryFilter, setCategoryFilter] = useState('All');
+    const [brandFilter, setBrandFilter] = useState('All');
+
+    // We use useEffect to call getCategories and getCategoriesAndBrands functions 
+    // when the component mounts.
+    useEffect(() => { getCategories(); getCategoriesAndBrands(); }, []);
+
+    // Functions to update categoryFilter and brandFilter respectively 
+    // when the user selects a different category or brand.
+    const handleCategories = (e) => {
+        setCategoryFilter(e.target.value);
+    }
+    const handleBrands = (e) => {
+        setBrandFilter(e.target.value);
+    }
+
+    // We call onFilter when categoryFilter or brandFilter change.
+    useEffect(() => {
+        onFilter({ category: categoryFilter, brand: brandFilter });
+    }, [categoryFilter, brandFilter]);
+
+    return (
+        <div className='filter'>
+            <span className='filter-text'>Filter Products:</span>
+
+            {/* Category selector */}
+            <select className='select' onChange={handleCategories}>
+                <option disabled selected>Category:</option>
+                <option>All</option>
+                {categories.map((category, index) => {
+                    return <option key={index}>{category}</option>
+                })}
+            </select>
+
+            {/* Brand selector */}
+            {categoryFilter && (categoryFilter !== "All") ? (
+                // If the user selects a category, we show the brands of that category.
+                <select className='select' onChange={handleBrands}>
+                    <option disabled selected>Brand:</option>
+                    <option>All</option>
+                    {/* 
+                    We search on {categoriesAndBrands} what matches with the value of {categoryFilter},
+                    we transform it into an array so we can map it and return an option for
+                    each brand of that category.
+                    */}
+                    {Array.from(categoriesAndBrands[categoryFilter]).map((brand, index) => {
+                        return <option key={index}>{brand}</option>
+                    })}
+                </select>
+            ) : (
+                // If the user selects "All" in the category selector, we show all brands.
+                <select className='select' onChange={handleBrands}>
+                    <option disabled selected>Brand:</option>
+                    <option>All</option>
+                    {brands.map((brand, index) => {
+                        return <option key={index}>{brand}</option>
+                    })}
+                </select>
+            )}
+        </div>
+    );
+}
--- frontend/src/pages/ProductList.js
+++ frontend/src/pages/ProductList.js
@@ -2,6 +2,7 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Products from '../components/Products';
 import CategoriesNavbar from '../components/CategoriesNavbar';
+import FilterNavbar from '../components/FilterNavbar';
 
 import './styles/ProductList.css';
 
@@ -11,20 +12,17 @@ import { useProducts } from '../context/ProductsContext.js';
 export default function ProductList() {
 
     // Import what we need from context.
-    const {getCategoriesAndBrands, categoriesAndBrands, brands, getCategories, categories} = useProducts();
+    const {getCategoriesAndBrands, getCategories} = useProducts();
 
     useEffect(() => { getCategories(); getCategoriesAndBrands(); }, []);
     
+    const [sort, setSort] = useState('newest');
     const [categoryFilter, setCategoryFilter] = useState('All');
     const [brandFilter, setBrandFilter] = useState('All');
-    const [sort, setSort] = useState('newest');
 
-    // Functions that change the value of the filters when other option is selected.
-    const handleCategories = (e) => {
-        setCategoryFilter(e.target.value);
-    }
-    const handleBrands = (e) => {
-        setBrandFilter(e.target.value);
+    const handleFilter = ({ category, brand }) => {
+        setCategoryFilter(category);
+        setBrandFilter(brand);
     }
 
     return (
@@ -40,51 +38,8 @@ export default function ProductList() {
                 <div className='filter-container'>
 
                     {/* Filters */}
-                    <div className='filter'>
-
-                        <span className='filter-text'>Filter Products:</span>
-
-                        {/* Category selector */}
-                        <select className='select' onChange={handleCategories}>
-                            <option disabled selected>Category:</option>
-                            <option>All</option>
-                            {/* We go through the categories array and we return an option for each category */}
-                            {categories.map((category, index) => {
-                                return <option key={index}>{category}</option>
-                            })}
-                        </select>
-
-                        {/* Brand selector */}
-                        {/* If there isn't a category selected, brand selector won't be shown */}
-                        {categoryFilter && (categoryFilter !== "All") ? (
-
-                            <select className='select' onChange={handleBrands}>
-                                <option disabled selected>Brand:</option>
-                                <option>All</option>
-                                {/* 
-                                We search on {categoriesAndBrands} what matches with the value of {categoryFilter},
-                                we transform it into an array so we can map it and return an option for
-                                each brand of that category.
-                                */}
-                                {Array.from(categoriesAndBrands[categoryFilter]).map((brand, index) => {
-                                    return <option key={index}>{brand}</option>
-                                })}
-
-                            </select>
-
-                        ) : (
-
-                            <select className='select' onChange={handleBrands}>
-                                <option disabled selected>Brand:</option>
-                                <option>All</option>
-
-                                {brands.map((brand, index) => {
-                                    return <option key={index}>{brand}</option>
-                                })}
-                            </select>
-                        )}
-
-                    </div>
+                    {/* We send useProducts to the component so it can use context logic*/}
+                    <FilterNavbar useProducts={useProducts} onFilter={handleFilter} />
 
                     <div className='sort'>
                         {/* Sort */}
