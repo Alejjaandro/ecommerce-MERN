a67ef7647cc0d50245db8f1dbf42e0d85ba8fc62
Alejjaandro <alejandroolaso98@gmail.com>
2023-10-28 10:02:53 +0200

    User controller corrections

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,7 +9,7 @@ export const getUser = async (req, res) => {
 
     try {
         // We find the user by its id.
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.userId);
 
         // In this case we don't want to show the password.
         const { password, ...noPassword } = user._doc;
@@ -24,18 +24,17 @@ export const getUser = async (req, res) => {
 export const updateUser = async (req, res) => {
 
     // We check if the body info it's the same that the one in the DB.
-    const user = await User.findById(req.params.id);
-    if (req.body.email === user.email) {
+    const user = await User.findById(req.params.userId);
+    if (req.body.email && (req.body.email === user.email)) {
         return res.status(400).json({ message: ['New Email cannot be the same as the old Email.'] });
     }
     // Check if the new email already exists in the database
     const emailExists = await User.findOne({ email: req.body.email });
     if (emailExists) { return res.status(400).json({ message: ['This email is not avaiable.'] }) }
 
-    if (req.body.username === user.username) {
+    if (req.body.username && (req.body.username === user.username)) {
         return res.status(400).json({ message: ['New Username cannot be the same as the old Username.'] });
     }
-
     // Check if the new username already exists in the database
     const usernameExists = await User.findOne({ username: req.body.username });
     if (usernameExists) { return res.status(400).json({ message: ['This Username is not avaiable.'] }) }
@@ -52,7 +51,7 @@ export const updateUser = async (req, res) => {
     // We find the user by its id and we set all the values that contains the body to modify.
     // The $set operator replaces the value of a field with the specified value.
     try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
             $set: req.body
         }, { new: true })
 
@@ -65,9 +64,10 @@ export const updateUser = async (req, res) => {
 
         // Create a new token in a cookie.
         res.cookie('token', newToken, { httpOnly: false });
-        return res.status(200).json({ message: 'User info Updated', updatedUser: updatedUser });
+        return res.status(200).json({ message: 'User info Updated', updatedUser: updatedUser, newToken: newToken });
 
     } catch (error) {
+        console.log(error);
         return res.status(500).json(error);
     }
 };
@@ -77,12 +77,12 @@ export const deleteUser = async (req, res) => {
 
     try {
         // Delete the user's cart
-        await Cart.findOneAndDelete({ _id: req.params.id });
+        await Cart.findOneAndDelete({ _id: req.params.userId });
         // Delete all orders that contain the userId.
-        await Order.deleteMany({ userId: req.params.id });
+        await Order.deleteMany({ userId: req.params.userId });
 
         // // We find the user by its id and we delete it.
-        await User.findByIdAndDelete(req.params.id);
+        await User.findByIdAndDelete(req.params.userId);
         return res.status(200).json('User Deleted');
 
     } catch (error) {
