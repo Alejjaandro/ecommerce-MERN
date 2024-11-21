ea5b71be870f0b8cddcde4c049792f515ed4dcf4
Alejjaandro <alejandroolaso98@gmail.com>
2024-11-21 17:10:50 +0100

    added header for petitions

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,9 +9,11 @@ const instance = axios.create({
 // Add a request interceptor
 instance.interceptors.request.use((config) => {
     // Do something before request is sent
+    // Access-Control-Allow-Origin: *
+    config.headers['Access-Control-Allow-Origin'] = '*';
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
-  }, (error) => {
+}, (error) => {
     // Do something with request error
     return Promise.reject(error);
 });
